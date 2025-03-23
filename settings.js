2467d815ecfc49afbaf69791823fe60d11b1f1e3
ryeen12 <152992632+ryeen12@users.noreply.github.com>
2025-03-23 18:18:29 +0300

    Elfa

--- settings.js
+++ settings.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined ? 'SUPUN-MD=qRdE3SBK#3xUDGfCXKJ_gi9u7p4UfeoUc5qTgbG1JGaAKpZQ1W_I ' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? 'SUPUN-MD=SQ8xnKDb#ukidHKCqvSw8PYPmHiaByDeYvmNwE9hJF0P8u3OAZ9k' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '+256742810290' : process.env.OWNER_NUMBER,
